1c4300d66b97c75d13bd77929998175003f76e7d
= <eliascolon98@gmail.com>
2022-12-19 23:42:12 -0500

    se agrego swagger y docker

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 import getProducts from './routes/products.routes';
 import getRoles from './routes/rol.routes';
 import getUsers from './routes/user.routes';
@@ -10,12 +11,15 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-// app.use(express.request('Auth'));
 app.use(express.json());
+
 app.use(getProducts);
 app.use(getRoles);
 app.use(getUsers);
 app.use(getSales);
+
+const swaggerDocument: any = require('../swagger.json');
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 export default app;
